d67a206ee1d5adfba18cee08ffde1272261a580c
huiyunfei <526317393@qq.com>
2019-04-15 16:49:09 +0800

    添加德鲁伊数据源

---
+++ corejavademo/src/main/java/com/example/restdemo/config/DruidConfig.java
@@ -0,0 +1,54 @@
+package com.example.restdemo.config;
+
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by hui.yunfei@qq.com on 2019/4/15
+ */
+@SpringBootConfiguration
+public class DruidConfig {
+
+    /**
+     * 配置druid监控
+     * 配置一个管理后台的servlet
+     * 访问地址：http://localhost:8080/druid/
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean statViewServlet() {
+        ServletRegistrationBean bean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+        Map<String, String> initParameters = new HashMap<String, String>();
+        initParameters.put("loginUsername", "admin");//属性见：com.alibaba.druid.support.http.ResourceServlet
+        initParameters.put("loginPassword", "123456");
+        initParameters.put("allow", "");//默认允许所有
+        initParameters.put("deny", "");
+        bean.setInitParameters(initParameters);
+        return bean;
+    }
+
+    /**
+     * 配置一个web监控的filter
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean webStatFilter() {
+        FilterRegistrationBean filterBean = new FilterRegistrationBean();
+        filterBean.setFilter(new WebStatFilter());
+        filterBean.setUrlPatterns(Arrays.asList("/*"));
+        System.out.println("进入web监控filter");
+        Map<String, String> initParameters = new HashMap<String, String>();
+        initParameters.put("exclusions", "*.js,*.css,/druid/*");//属性见：com.alibaba.druid.support.http.WebStatFilter
+        filterBean.setInitParameters(initParameters);
+
+        return filterBean;
+    }
+}
--- corejavademo/src/main/java/com/example/restdemo/web/TestUserWeb.java
+++ corejavademo/src/main/java/com/example/restdemo/web/TestUserWeb.java
@@ -11,6 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  * Created by huiyunfei on 2019/4/12.
  */
@@ -43,6 +47,22 @@ public class TestUserWeb {
         return result;
     }
 
+
+    @Autowired
+    private DataSource dataSource;
+
+    @RequestMapping("/dataSource")
+    public String dataSource() {
+        try {
+            System.out.println("dataSource = "+dataSource);
+            Connection conn = dataSource.getConnection();
+            System.out.println("conn = "+conn);
+            return "success";
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "end.";
+    }
 }
 
 
