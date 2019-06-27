4cf13bdc680bb8482daaa82f619198b584b8887c
huiyunfei <526317393@qq.com>
2019-06-27 17:55:32 +0800

    添加Redis锁

--- corejavademo/src/test/java/com/example/rabbitmq/direct/HelloSenderTest.java
+++ corejavademo/src/test/java/com/example/rabbitmq/direct/HelloSenderTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class HelloSenderTest {
     @Autowired
     private AmqpTemplate amqpTemplate;
+    //需要将一个队列绑定到交换机上，要求该消息与一个特定的路由键完全匹配
     @Test
     public void direct(){
         User user=new User();
@@ -22,10 +23,12 @@ public class HelloSenderTest {
         user.setName("yunfei");
         amqpTemplate.convertAndSend("hello2",user);
     }
+    //将路由键和某模式进行匹配。此时队列需要绑定要一个模式上。符号“#”匹配一个或多个词，符号“*”匹配不多不少一个词
     @Test
     public void topic(){
         amqpTemplate.convertAndSend("exchange","topic.message","123");
     }
+    //不处理路由键。你只需要简单的将队列绑定到交换机上。一个发送到交换机的消息都会被转发到与该交换机绑定的所有队列上
     @Test
     public void fanout(){
         amqpTemplate.convertAndSend("fanoutExchange","","123");
